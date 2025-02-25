fbdaef1b28a9941da5cdf4839683d31ecb15d6a5
KARLA_ALEJANDRA_BALDERRAMA_PADILLA <a.22308051281149@cbtis128.edu.mx>
2025-02-25 16:58:45 +0000

    balderrama imagen

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,73 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MiImagen());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiImagen extends StatelessWidget {
+  const MiImagen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Mis Imagenes",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          backgroundColor: const Color(0xff6ba0ca),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Balderrama Karla  MAT. 22308051281149',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              Image.network(
+                'https://raw.githubusercontent.com/Balderrama-Karla-5J/Imagenes-para-APP-flutter-6J/refs/heads/main/libro1.jpg', // URL de la primera imagen
+                height: 200,
+                width: 150,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(height: 30), // Espacio entre las imágenes
+              const Text(
+                'Nuestra señora en paris de github',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(height: 30), // Espacio entre las imágenes
+              Image.network(
+                'https://raw.githubusercontent.com/Balderrama-Karla-5J/Imagenes-para-APP-flutter-6J/refs/heads/main/libro-3.jpg', // URL de la segunda imagen
+                height: 200,
+                width: 150,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(height: 30), // Espacio entre las imágenes
+              const Text(
+                'George Orwell - 1984 de github',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    );
-  }
-}
+    ); //material app
+  } //Widget
+} //clase MiImagen
